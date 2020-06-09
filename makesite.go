e3f0b261cd143f9224c6ebd0b77ce2cf707a14dd
Luke <deandrevidal@aol.com>
2020-06-09 12:08:40 -0700

    edit template and sace html file

--- makesite.go
+++ makesite.go
@@ -1,17 +1,59 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"flag"
+	"html/template"
 	"io/ioutil"
+	"strings"
 )
 
-func main() {
+type post struct {
+	User    string
+	Content string
+}
+
+func readFile(fileName string) string {
 	fileContents, err := ioutil.ReadFile("first-post.txt")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Print(string(fileContents))
 
+	return string(fileContents)
 }
 
-// func renderTemplate()
+func renderTemplate(content string) string {
+	paths := []string{
+		"template.tmpl",
+	}
+
+	buff := new(bytes.Buffer)
+	t := template.Must(template.New("template.tmpl").ParseFiles(paths...))
+	err := t.Execute(buff, post{User: "Luke", Content: content})
+	if err != nil {
+		panic(err)
+	}
+
+	return buff.String()
+}
+
+func saveFile(buffer string, fileName string) bool {
+	bytesToWrite := []byte(buffer)
+	err := ioutil.WriteFile(fileName, bytesToWrite, 0644)
+
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+func main() {
+	filePtr := flag.String("file", "first-post.txt", "name of file contents to read")
+	flag.Parse()
+
+	content := readFile(*filePtr)
+	template := renderTemplate(content)
+	fileName := strings.SplitN(*filePtr, ".", 2)[0] + ".html"
+	saveFile(template, fileName)
+}
